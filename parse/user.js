f514cb2f8e8337b8c2109944b5958002132f3318
wingleungchoi <wingleungchoi@gmail.com>
2022-06-15 10:45:23 +0100

    benachmark remove unused examples

--- parse/user.js
+++
@@ -1,9 +0,0 @@
-const user = {
-  "name": "Jane Doe",
-  "favorite-game": "Stardew Valley",
-  "subscriber": false
-}
-
-const userStr = '{"name":"Jane Doe","favorite-game":"Stardew Valley"}';
-exports.user = user;
-exports.userStr = userStr;
